eb53c4eabffda1010b9c897927d04ff0c3a01ef9
fleawu-pegatroncorp <flea_wu@pegatroncorp.com>
2026-08-04 22:06:18 +0800

    FirstRelease

---
+++ ML_PostProcess_1.00a_20260804/Tools/post_process_single_no_audio.bat
@@ -0,0 +1,3 @@
+post_process.exe --debug --skip_audio=True --config=robocalv4_config_pre_post.yaml %1 %2 %3
+echo %errorlevel%
+exit /b %errorlevel%
---
+++ _Online_PostProcess.bat
@@ -0,0 +1,5 @@
+@echo off
+Title ML_PostProcess_Online_Mode
+SET SFISCONN=True
+SET FIXCTL=True
+call start.bat PostProcess
